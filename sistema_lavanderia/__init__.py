c559b6011d25dfd0908e3d6e2c241b2bd66baf39
Jostyn Alcivar <80085733+jostynalcivar13@users.noreply.github.com>
2025-01-14 21:56:58 -0500

    Update __init__.py

--- sistema_lavanderia/__init__.py
+++ sistema_lavanderia/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 
-db = SQLAlchemy()
+mysql = MySQL()
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config['MYSQL_HOST'] = 'mysql-sofuer-porto-car-wash.g.aivencloud.com'
+    app.config['MYSQL_USER'] = 'avnadmin'
+    app.config['MYSQL_PASSWORD'] = 'AVNS_USU_gdEw2HXwwO8UhFQ'
+    app.config['MYSQL_DB'] = 'defaultdb'
+    app.config['MYSQL_PORT'] = 19682
+    app.config['MYSQL_SSL_MODE'] = 'REQUIRED'
+    
     app.secret_key = 'grupopatito'
-
+    
+    # Inicializar MySQL
+    mysql.init_app(app)
+    
     with app.app_context():
         from .routes import routes
         app.register_blueprint(routes)
